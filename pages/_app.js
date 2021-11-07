8b8bf57bf4f1de9bc296a02e7f70e065221cb66b
Jakub Kropáček <kropikuba@gmail.com>
2021-11-07 15:14:00 +0100

    :tada:Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function HomeLayout({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default HomeLayout
