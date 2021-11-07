4af92e304cae3455aed10223b80b39c2f1108e1a
Jakub Kropáček <kropikuba@gmail.com>
2021-11-07 21:11:55 +0100

    :sparkles: Removed Dynamic Route, changed Home page

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,15 @@
 import styles from '../styles/Layout.module.css'
-import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Layout({ user, children }) {
+export default function Layout({ children }) {
   return (
     <div>
       <div className={styles.navbar}>
-        <Link href={user.html_url} passHref>
-          <p className={styles.link}>Odkaz 1</p>
+        <Link href="/" passHref>
+          <p className={styles.link}>Home</p>
         </Link>
-        <Link href={user.html_url} passHref>
-          <p className={styles.link}>Odkaz 2</p>
+        <Link href="/repos" passHref>
+          <p className={styles.link}>Projects</p>
         </Link>
       </div>
       <div className={styles.content}>{children}</div>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,58 @@
-import React from 'react'
+import style from '../styles/Home.module.css'
+import Layout from '../components/layout.js'
 
 export default function Home() {
   return (
-    <div>
-      <h1>The Basic Site</h1>
-      <p>Be prepared</p>
-    </div>
+    <Layout>
+      <div>
+        <h1>JustScreaMy</h1>
+        <p>
+          Hello! My name is Jakub Kropáček and I&apos;m from Czech Republic.
+        </p>
+        <p>
+          I have been interested in programming since my primary school, after I
+          saw a Scratch icon on my desktop in computer science class. That
+          didn&apos;t last long, after few days I found out about JavaScript on
+          Youtube. And that is why I chose the path I&apos;m going on right now.
+        </p>
+        <p>
+          I chose Technical School of Electrical Engineering and Computer
+          Science as my secondary school (which I&apos;m still studying by now)
+        </p>
+        <p>
+          I&apos;m mostly interested in Networking Technologies and
+          Cybersecurity but that is only a plan for a university in my future.
+        </p>
+        <p>
+          What I do right now is programming mostly for fun or trying to
+          contribute on Open Source projects. I love learning and trying new
+          technologies
+        </p>
+        <p>
+          Here are some languages I have tried and been able to do something
+          with them:
+          <ul>
+            <li>Python</li>
+            <li>JavaScript</li>
+            <li>C</li>
+            <li>Go (Least knowledge)</li>
+            <li>PHP (Little more knowledge than Go)</li>
+            <li>HTML/CSS (I don&apos;t like CSS tho)</li>
+            <li>SQL</li>
+          </ul>
+        </p>
+        <p>
+          Other skills and knowledge:
+          <ul>
+            <li>Git</li>
+            <li>Next.js</li>
+            <li>Flask</li>
+            <li>Arduino</li>
+            <li>Cisco IOS (Still learning)</li>
+            <li>Linux (Still learning)</li>
+          </ul>
+        </p>
+      </div>
+    </Layout>
   )
 }
